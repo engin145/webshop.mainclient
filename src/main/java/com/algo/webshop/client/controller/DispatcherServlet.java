3b83b410a929b9688364dba394cb5ff26204f4f7
vitaliy <engin145@mail.ru>
2013-09-28 20:56:54 +0300

    first commit

---
+++ src/main/java/com/algo/webshop/client/controller/DispatcherServlet.java
@@ -0,0 +1,13 @@
+package com.algo.webshop.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DispatcherServlet {
+
+	@RequestMapping({ "/", "/index" })
+	public String index() {
+		return "index";
+	}
+}
